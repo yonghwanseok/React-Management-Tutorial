f84c5f8ca4c003e1e5aab9af93d63930e999a70a
HwanSeok Yong <101648597+yonghwanseok@users.noreply.github.com>
2023-03-27 23:33:04 +0900

    수정2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render(){
     return (
       <div className="grey-background">
-        <img src={logo} lat="log"/>
+        <img src={logo} lat="logo"/>
         <h2>Let's develop management system</h2>
       </div>
     );
